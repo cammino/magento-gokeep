113bb0bd606825f3160053ec3493297ed899cb59
Marcio Junior <marcio@cammino.com.br>
2016-06-14 14:52:43 -0300

    adiciona estrutura basica para o modulo

---
+++ Block/Tracking.php
@@ -0,0 +1,10 @@
+<?php
+class Gokeep_Tracking_Block_Tracking extends Mage_Core_Block_Template {
+	// protected function _toHtml() {
+	// }
+
+	public function getTrackingCode() {
+		return "hello from block";
+	}
+}
+?>
