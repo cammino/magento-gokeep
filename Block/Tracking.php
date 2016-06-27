9caf355db406a4ade545cc2266726f789976e2c3
Lucas Guarnieri <lucas.guarnieri.s@gmail.com>
2016-06-27 11:32:16 -0300

    Atualiza comentario do metodo de verificacao se é uma pagina de busca

--- Block/Tracking.php
+++ Block/Tracking.php
@@ -507,7 +507,7 @@ class Gokeep_Tracking_Block_Tracking extends Mage_Core_Block_Template
     }
 
     /**
-    * Check if is page checkout/onepage
+    * Check if is page catalogsearch/result
     *
     * @return bool
     */
